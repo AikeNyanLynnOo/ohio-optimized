cf4df571351bc0219b4f2a896068c782c5e3a364
AikeNyanLynnOo <aikenyanlynnoo2399@gmail.com>
2021-08-08 16:08:21 +0630

    Css,js files added

---
+++ assets/js/style.js
@@ -0,0 +1,14 @@
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar");
+    var $navbrand = $(".navbar-brand");
+    // $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    $navbrand.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
+
+function fullScreenImage(element) {
+  document.getElementById("modalImage").src = element.src;
+  document.getElementById("imagePopup").style.display = "block";
+}
