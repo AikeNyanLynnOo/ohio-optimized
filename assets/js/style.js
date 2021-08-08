e8f4f7e0bdcc7aa6d6cf733a81512c70b0d696db
MayPhyoKyaw <MayPhyoKyaw@users.noreply.github.com>
2021-08-08 17:42:16 +0630

    add back to top button

--- assets/js/style.js
+++ assets/js/style.js
@@ -12,3 +12,18 @@ function fullScreenImage(element) {
   document.getElementById("modalImage").src = element.src;
   document.getElementById("imagePopup").style.display = "block";
 }
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
